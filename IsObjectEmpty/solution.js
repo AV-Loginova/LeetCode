0724b76d660927d5490992cefb370aaeadee3468
Alena <alena@MacBook-Air-Alena.local>
2023-09-18 17:24:46 +0300

    delete

--- IsObjectEmpty/solution.js
+++
@@ -1,7 +0,0 @@
-var isEmpty = function(obj) {
-    if (Object.keys(obj).length == 0) {
-        return true
-    } else {
-        return false
-    }
-};
--- README.md
+++
@@ -1,5 +0,0 @@
-# LeetCode
-
-## The list of tasks:
-1) Create Hello World Function
-2) Array Prototype Last
